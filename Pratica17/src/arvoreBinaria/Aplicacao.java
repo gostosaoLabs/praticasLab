a942c03a5a8e15dfe7c33e8be3b3a5201620eba8
willerhiago <willer.hiago@bol.com.br>
2017-11-20 15:02:01 -0200

    Att

--- Pratica17/src/arvoreBinaria/Aplicacao.java
+++ Pratica17/src/arvoreBinaria/Aplicacao.java
@@ -14,9 +14,11 @@ public class Aplicacao {
 		tree.insere(6);
 		tree.insere(5);
 		tree.remove(5);
-	//	tree.remove(10);
-	//	tree.remove(8);
-/*	System.out.println("Pré-ordem:");
+		tree.remove(10);
+		tree.remove(8);
+		
+		
+	System.out.println("Pré-ordem:");
 	tree.preOrdem();
 	System.out.println();
 	System.out.println("Pós-ordem:");
@@ -27,28 +29,20 @@ public class Aplicacao {
 	System.out.println();
 	System.out.println("Largura:");
 	tree.largura();
+	
+	
 	System.out.println("\nAltura do nó [6]: "+tree.pesquisaNo(6).getAlturaNo());
-	*/
-		tree.largura();
+	
+	
 	System.out.println("Profundidade do nó [6]: "+tree.pesquisaNo(6).getProfundidadeNo());
 	
-	System.out.println(tree.getAltura());
-	//System.out.println(tree.getProfundidade());
+	System.out.println("Altura árvore: "+tree.getAltura());
+	System.out.println("Profundidade árvore: "+tree.getProfundidade());
 	
-	//System.out.println(tree.existeNaArvore(10));
+	System.out.println("Nodo 10 existe na arvore: "+tree.existeNaArvore(10));
 	tree.salvarArquivo();
 
-	//System.out.println("Altura[4]: "+tree.pesquisaNo(4).getAlt());
-	//System.out.println("balanceado[5]: "+tree.pesquisaNo(5).balanceado());
-	//System.out.println("balanceado[4]: "+tree.pesquisaNo(4).balanceado());
-	//System.out.println("balanceado[3]: "+tree.pesquisaNo(3).balanceado());
-	//System.out.println(tree.pesquisaNo(5).balanceado());
-	
-	/*gravar.gravar("==========Arvore========== ");
-	gravar.gravar("Nodos: "+result);
-	gravar.gravar("Altura da árvore: "+this.getAltura());
-	gravar.gravar("Profundidade da árvore: "+this.getProfundidade());
-*/
+
 	}
 
 }
