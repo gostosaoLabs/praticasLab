3aa8c9dbb17453852592b9f214b3579f4ba8f2cf
willerhiago <willer.hiago@bol.com.br>
2017-11-20 22:59:27 -0200

    remove

--- Pratica5/src/recursividade/Aplicacao.java
+++
@@ -1,9 +0,0 @@
-package recursividade;
-
-public class Aplicacao {
-
-	public static void main(String args[]) {
-		Potenciacao pot = new Potenciacao();
-		System.out.println(pot.potenciacao(3, 3));
-	}
-}
